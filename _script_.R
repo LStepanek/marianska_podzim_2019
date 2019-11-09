2df6afd80ba14178c93b4f2cb3c5040019446ed5
Lubomír Štěpánek <lubomir.stepanek@gmail.com>
2019-11-09 17:47:25 +0100

    Add files via upload

--- _script_.R
+++ _script_.R
@@ -12,6 +12,7 @@ invisible(
             "data.table",
             "parallel",
             "snow",
+            "lme4",
             "future",
             "Rcpp"
         ),
@@ -381,6 +382,204 @@ for(my_vector_name in c("x", "y")){
         cat("\n")
     }
 }
+
+
+## ----------------------------------------------------------------------------
+
+###############################################################################
+
+## pokročilý caching funkcí a proměnných --------------------------------------
+
+# původní funkce
+getFibonacci <- function(n){
+    if(n == 1){return(1)}
+    if(n == 2){return(1)}
+    if(n >= 3){
+        return(
+            getFibonacci(n - 1) +
+            getFibonacci(n - 2)
+        )
+    }
+}
+
+# cachovaná funkce
+memoisedFibonacci <- memoise::memoise(getFibonacci)
+
+
+# benchmarking
+n <- 25
+
+microbenchmark::microbenchmark(
+    times = 10,
+    unit = "ms",
+    getFibonacci(n),
+    memoisedFibonacci(n)
+)
+
+
+## ----------------------------------------------------------------------------
+
+###############################################################################
+
+## prekompilace ---------------------------------------------------------------
+
+library(compiler)
+
+getFunction("mean")
+# function (x, ...) 
+# UseMethod("mean")
+# <bytecode: 0x033be6b8>
+# <environment: namespace:base>
+
+my_mean <- function(x){
+    my_output <- 0
+    for(i in 1:length(x)){
+        my_output <- my_output + x[i] / length(x)
+    }
+    return(my_output)
+}
+
+# kompilace pomocí funkce cmpfun() balíčku compiler
+compiled_my_mean <- compiler::cmpfun(my_mean)
+
+x <- rnorm(1000)
+microbenchmark::microbenchmark(
+    times = 100,
+    unit = "ms",
+    mean(x), my_mean(x), compiled_my_mean(x)
+)
+# Unit: milliseconds
+#                 expr      min        lq       mean    median       uq      max
+#              mean(x) 0.005133 0.0059870 0.00674457 0.0062015 0.006843 0.033357
+#           my_mean(x) 0.163788 0.1669955 0.21073893 0.1702020 0.175762 4.044222
+#  compiled_my_mean(x) 0.163361 0.1676370 0.17462025 0.1697750 0.175762 0.234349
+#  neval cld
+#    100  a 
+#    100   b
+#    100   b
+
+
+## ----------------------------------------------------------------------------
+
+###############################################################################
+
+## načítání velkých souborů ---------------------------------------------------
+
+my_data <- data.frame(matrix(1:1e7, nrow = 1e6))
+
+library(data.table)
+fwrite(x = my_data, file = "moje_data.txt")
+fread("moje_data.txt")
+
+library(vroom)
+vroom_write(x = my_data, path = "moje_data.txt")
+vroom("moje_data.txt")
+
+## ----------------------------------------------------------------------------
+
+###############################################################################
+
+## paralelizace ---------------------------------------------------------------
+
+library(parallel)
+library(MASS)
+
+
+# zkusme
+clusterEvalQ(my_cluster, 2 + 2)
+
+x <- 1
+clusterEvalQ(my_cluster, x)
+
+rm(y)
+clusterEvalQ(my_cluster, y <- 1)
+clusterEvalQ(my_cluster, y)
+y
+
+clusterEvalQ(
+    my_cluster,
+    {
+        library(ggplot2)
+        library(stringr)
+    }
+)
+
+parSapply(my_cluster, Orange, mean, na.rm = TRUE)
+
+stopCluster(my_cluster)
+
+
+#### komplexní případ
+
+###### lapply verze na jednom jádru
+
+library(lme4)
+
+my_function <- function(i){
+    lmer(
+        Petal.Width ~ . - Species + (1 | Species),
+        data = iris
+    )
+}
+ 
+system.time(my_first_save <- lapply(1:100, my_function))
+
+
+###### spoužitím paralelizované verze lapply() (na jednom jádru)
+
+system.time(my_second_save <- mclapply(
+    1:100,
+    my_function,
+    mc.cores = detectCores() # ne na Windows !
+))
+
+
+###### na více jádrech
+
+my_cluster <- makeCluster(detectCores())
+clusterEvalQ(my_cluster, library(lme4))
+
+system.time(my_third_save <- parLapply(my_cluster, 1:100, my_function))
+
+stopCluster(my_cluster)
+
+
+## ----------------------------------------------------------------------------
+
+###############################################################################
+
+## C++ v R --------------------------------------------------------------------
+
+library(Rcpp)
+
+cppFunction(
+    'int signCpp(double x) {
+        if (x > 0) {
+            return 1;
+        } else if (x == 0) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }'
+)
+
+signCpp(-4)
+signCpp(0)
+signCpp(10)
+
+
+cppFunction(
+    'double mean_cpp(NumericVector x) {
+        int i;
+        int n = x.size();
+        double mean = 0;
+        for(i=0; i<n; i++) {
+            mean = mean + x[i] / n;
+        }
+        return mean;
+    }'
+)
 
 
 ## ----------------------------------------------------------------------------
